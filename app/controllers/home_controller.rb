32a4bae339945fae9c96e0a472a7a9ca8b11057b
Roger Tinsley <mail@rogertinsley.co.uk>
2016-02-28 20:58:56 +0000

    Removed two actions that are not required

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,4 @@ class HomeController < ApplicationController
     @questions = Question.order(created_at: :desc).all
   end
 
-  def about
-  end
-
-  def temp
-    redirect_to root_path
-  end
-
 end
